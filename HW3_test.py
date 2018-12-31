4c098a32287cab45d5352cf69ccb4cf26e75ffd5
idoglanz <idoglanz@gmail.com>
2018-12-31 10:32:35 +0200

    With word doc

--- HW3_test.py
+++ HW3_test.py
@@ -81,11 +81,10 @@ print(X_train.shape)
 
 
 input_shape = [32, 32, 3]
-
 num_classes = 10
+
 batch_size = 128
 epochs = 10
-
 
 model = Sequential()
 
@@ -101,7 +100,7 @@ model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
-model.add(Dense(1024, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
